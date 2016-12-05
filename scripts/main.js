e53f83501ad78172751f9d93a02e8da6082e35e1
Øistein Haugland <haugland.1193@gmail.com>
2016-12-05 23:32:11 +0100

    added css-tricks, and jquery Hooks. shading

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,63 @@
 /*******************************
 FUNCTIONS
 ******************************/
+
+//A cssHook from jquery that makes css(backgrounrColor) return as hex instead of rgb
+$.cssHooks.backgroundColor = {
+    get: function(elem) {
+        if (elem.currentStyle)
+            var bg = elem.currentStyle["backgroundColor"];
+        else if (window.getComputedStyle)
+            var bg = document.defaultView.getComputedStyle(elem,
+                null).getPropertyValue("background-color");
+        if (bg.search("rgb") == -1)
+            return bg;
+        else {
+            bg = bg.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+            function hex(x) {
+                return ("0" + parseInt(x).toString(16)).slice(-2);
+            }
+            return "#" + hex(bg[1]) + hex(bg[2]) + hex(bg[3]);
+        }
+    }
+}
+
+
+
+
 var squaresPerRow = 5;
+
+// function for darkening , cudos css-tricks
+function LightenDarkenColor(col, amt) {
+  
+    var usePound = false;
+  
+    if (col[0] == "#") {
+        col = col.slice(1);
+        usePound = true;
+    }
+ 
+    var num = parseInt(col,16);
+ 
+    var r = (num >> 16) + amt;
+ 
+    if (r > 255) r = 255;
+    else if  (r < 0) r = 0;
+ 
+    var b = ((num >> 8) & 0x00FF) + amt;
+ 
+    if (b > 255) b = 255;
+    else if  (b < 0) b = 0;
+ 
+    var g = (num & 0x0000FF) + amt;
+ 
+    if (g > 255) g = 255;
+    else if (g < 0) g = 0;
+ 
+    return (usePound?"#":"") + (g | (b << 8) | (r << 16)).toString(16);
+  
+}
+
 function generateGrid(numSquares, option){
 	for(var i  = 0; i < numSquares * numSquares; i++)
 	{		
@@ -11,11 +67,21 @@ function generateGrid(numSquares, option){
 	var width = ($("#grid").width())/ squaresPerRow;  
 	
 	$(".box").css({"width":width ,"height":width});	
+	
 	$(".box").on('mouseenter', function(){
-		$(this).css("background", getRandomColor());
-	});
+		if($(this).hasClass("triggered")){
+			var bgColor = $(this).css("backgroundColor");
+			var newColor = LightenDarkenColor(bgColor, -20); 
+			$(this).css("backgroundColor", newColor);
+		}
+		else {
+			$(this).css("backgroundColor", getRandomColor());
+			$(this).addClass("triggered");
+		}
 		
-};
+		});
+	};
+		
 
 function updateGrid(){
 	$(".box").remove();
@@ -38,7 +104,7 @@ function getRandomColor() {
     for (var i = 0; i < 6; i++ ) {
         color += letters[Math.floor(Math.random() * 16)];
     }
-
+    getRandomColor.called = true;
     return color;
 }
 
